85d45036c5f2b811ed9b9832501db2be8b4488fd
oleh-turchyn <47058088+Leggi19@users.noreply.github.com>
2020-04-18 12:23:52 +0300

    completed adding json for tours

--- js/Nidavelir.js
+++ js/Nidavelir.js
@@ -11,77 +11,77 @@
 
 let elem = document.querySelector("#text");
 let text = document.querySelector(".about__text");
-elem.onmouseover = function(){
-    this.style.backgroundColor = "black";
-    text.style.color = "white";
+elem.onmouseover = function () {
+	this.style.backgroundColor = "black";
+	text.style.color = "white";
 };
 
-elem.onmouseout = function(){
-    this.style.backgroundColor = "white";
-    text.style.color="#999";
+elem.onmouseout = function () {
+	this.style.backgroundColor = "white";
+	text.style.color = "#999";
 };
 
 
-$(document).ready(function(){
-    $("header").removeClass("default");
-    $(window).scroll(function() {
-        //var scrollPos = $(this).scrollTop();
-        if($(this).scrollTop()>20){
-            $("header").addClass("default");
-        }else{
-            $("header").removeClass("default");
-        }
-    
-    });
+$(document).ready(function () {
+	$("header").removeClass("default");
+	$(window).scroll(function () {
+		//var scrollPos = $(this).scrollTop();
+		if ($(this).scrollTop() > 20) {
+			$("header").addClass("default");
+		} else {
+			$("header").removeClass("default");
+		}
+
+	});
 
 });
 
-$(document).ready(function() {
-		$('a[href*=#]').bind('click', function(e) {
-				e.preventDefault(); // prevent hard jump, the default behavior
+$(document).ready(function () {
+	$('a[href*=#]').bind('click', function (e) {
+		e.preventDefault(); // prevent hard jump, the default behavior
 
-				var target = $(this).attr("href"); // Set the target as variable
+		var target = $(this).attr("href"); // Set the target as variable
 
-				// perform animated scrolling by getting top-position of target-element and set it as scroll target
-				$('html, body').stop().animate({
-						scrollTop: $(target).offset().top
-				}, 600, function() {
-						location.hash = target; //attach the hash (#jumptarget) to the pageurl
-				});
-
-				return false;
+		// perform animated scrolling by getting top-position of target-element and set it as scroll target
+		$('html, body').stop().animate({
+			scrollTop: $(target).offset().top
+		}, 600, function () {
+			location.hash = target; //attach the hash (#jumptarget) to the pageurl
 		});
+
+		return false;
+	});
 });
 
-$(window).scroll(function() {
-		var scrollDistance = $(window).scrollTop();
-        $('.nav a.active').removeClass('active');
-		// Show/hide menu on scroll
-		//if (scrollDistance >= 850) {
-		//		$('nav').fadeIn("fast");
-		//} else {
-		//		$('nav').fadeOut("fast");
-		//}
-	
-		// Assign active class to nav links while scolling
-		$('.section').each(function(i) {
-				if ($(this).position().top <= scrollDistance) {
-						$('.nav a.active').removeClass('active');
-						$('.nav a').eq(i).addClass('active');
-				}
-		});
+$(window).scroll(function () {
+	var scrollDistance = $(window).scrollTop();
+	$('.nav a.active').removeClass('active');
+	// Show/hide menu on scroll
+	//if (scrollDistance >= 850) {
+	//		$('nav').fadeIn("fast");
+	//} else {
+	//		$('nav').fadeOut("fast");
+	//}
+
+	// Assign active class to nav links while scolling
+	$('.section').each(function (i) {
+		if ($(this).position().top <= scrollDistance) {
+			$('.nav a.active').removeClass('active');
+			$('.nav a').eq(i).addClass('active');
+		}
+	});
 }).scroll();
 
-$(document).ready(function(){
-    $(".menu-toggle").click(function(){
-        $(".menu-toggle").toggleClass("active");
-        $(".nav").toggleClass("active");
-    });
+$(document).ready(function () {
+	$(".menu-toggle").click(function () {
+		$(".menu-toggle").toggleClass("active");
+		$(".nav").toggleClass("active");
+	});
 });
 
 
 
-$(function(){
+$(function () {
 	$('.reviews').slick({
 		nextArrow: '<button type="button" class="slick-btn slick_next"></button>',
 		prevArrow: '<button type="button" class="slick-btn slick_prev"></button>',
@@ -119,7 +119,7 @@ $(function(){
 // 			const tabsContainer = sideBar.parentElement;
 // 			const tabNumber = button.dataset.forTab;
 // 			const tabsToActivate = 	tabsContainer.querySelector('tabs_content[data-tab="${}"');
-			
+
 // 			sideBar.querySelectorAll(".tabs_button").forEach(button =>{
 // 				button.classList.remove(".tabs_button-active");
 // 			});
@@ -175,14 +175,15 @@ $(function(){
 
 
 
-+function () {
++
+function () {
 
 	document.querySelector('.tabs_button').classList.add('tabs_button-active');
 	document.querySelector('.tabs_content').classList.add('tabs_content-active');
-	
-	function selectPanel (e) {
+
+	function selectPanel(e) {
 		var target = e.target.dataset.target;
-	
+
 		document.querySelectorAll('.tabs_button').forEach(el => el.classList.remove('tabs_button-active'));
 		document.querySelectorAll('.tabs_content').forEach(el => el.classList.remove('tabs_content-active'));
 		e.target.classList.add('tabs_button-active');
@@ -190,19 +191,19 @@ $(function(){
 		console.log(document.querySelector('.' + target));
 		document.querySelector('.' + target).classList.add('tabs_content-active');
 	}
-	
+
 	document.querySelectorAll('.tabs_button').forEach(el => {
 		el.addEventListener('click', selectPanel);
 	});
-	
-	}()
+
+}()
 
 
-	Array.from(document.querySelectorAll('.navigation_button')).forEach((item)=>{
-		item.onclick = () =>{
-			item.parentElement.parentElement.classList.toggle('change');
-		}
-	});
+Array.from(document.querySelectorAll('.navigation_button')).forEach((item) => {
+	item.onclick = () => {
+		item.parentElement.parentElement.classList.toggle('change');
+	}
+});
 
 
 
@@ -211,41 +212,39 @@ var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
-request.onload = function() {
+request.onload = function () {
 	var poptours = request.response;
 	//populateHeader(superHeroes);
 	showTours(poptours);
-  };
-  var cards_wrapper = document.querySelector('.cards_wrapper');
-  var myCard = document.querySelectorAll('.card');
-  var myFrontSide = document.querySelectorAll('.front-side');
-  var myHeader = document.querySelectorAll('.tour_name');
-  function showTours(jsonObj) {
-	var tours = jsonObj['tours'];
-		
-	for (var i = 0; i < tours.length; i++) {
-	  
-	  //var myList = document.querySelector('.card_list');
-  
-	  myHeader[i].textContent = tours[i].name;
-	  myFrontSide[i].appendChild(myHeader[i]);
-	  myCard[i].appendChild(myFrontSide[i]);
-	  cards_wrapper.appendChild(myCard[i]);
-		  
-	//   var description = tours[i].description;
-	//   for (var j = 0; j < description.length; j++) {
-	// 	var listItem = document.querySelectorAll('.card_list-item');
-	// 	listItem.textContent = description[j];
-	// 	// myList.appendChild(listItem);
-	//   }
-  
-	//   myArticle.appendChild(myH2);
-	//   myArticle.appendChild(myPara1);
-	//   myArticle.appendChild(myPara2);
-	//   myArticle.appendChild(myPara3);
-	//   myArticle.appendChild(myList);
-  
-	//   section.appendChild(myArticle);
-	}
-  }
+};
+var cards_wrapper = document.querySelector('.cards_wrapper');
+var myCard = document.querySelectorAll('.card');
+var myFrontSide = document.querySelectorAll('.front-side');
+var myHeader = document.querySelectorAll('.tour_name');
+var myList = document.querySelectorAll('.card_list');
 
+function showTours(jsonObj) {
+	var tours = jsonObj['tours'];
+
+	for (var i = 0; i < tours.length; i++) {
+		var description = tours[i].description;
+		for (var j = 0; j < description.length; j++) {
+			var listItem = document.querySelector('.card_list-item');
+			listItem.textContent = description[j];
+			myList[i].appendChild(listItem);
+		}
+
+		//   myArticle.appendChild(myH2);
+		//   myArticle.appendChild(myPara1);
+		//   myArticle.appendChild(myPara2);
+		//   myArticle.appendChild(myPara3);
+		//   myArticle.appendChild(myList);
+
+		//   section.appendChild(myArticle);
+		myHeader[i].textContent = tours[i].name;
+		myFrontSide[i].appendChild(myHeader[i]);
+		myFrontSide[i].appendChild(myList[i]);
+		myCard[i].appendChild(myFrontSide[i]);
+		cards_wrapper.appendChild(myCard[i]);
+	}
+}
